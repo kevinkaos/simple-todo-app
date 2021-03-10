ba9964c2eb5aa08e959c064b7aed83e0859b563b
Kevin Kao <namedkk126@gmail.com>
2021-03-10 22:45:39 +0800

    Updates

---
+++ precache-manifest.d27e479e9bcea5f6bd4aec928a6177a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f091de2de9a2d06b54f38c8b21f7fe31",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "2bad310efc61005eb673",
+    "url": "/simple-todo-app/static/css/main.c3bc9b70.chunk.css"
+  },
+  {
+    "revision": "efd4b65f52391067fd42",
+    "url": "/simple-todo-app/static/js/2.dba927fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.dba927fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bad310efc61005eb673",
+    "url": "/simple-todo-app/static/js/main.0d6771d0.chunk.js"
+  },
+  {
+    "revision": "3a8ace12cbaf03f9bb37",
+    "url": "/simple-todo-app/static/js/runtime-main.485db89b.js"
+  },
+  {
+    "revision": "74fc797d2d34f79f05a5255671eaeb25",
+    "url": "/simple-todo-app/static/media/add.74fc797d.svg"
+  },
+  {
+    "revision": "8a0c8c4e90d8ac7681629b333225fc86",
+    "url": "/simple-todo-app/static/media/search.8a0c8c4e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5474e6a5464b2e3a4ee07a067f64d113.js"
+  "/simple-todo-app/precache-manifest.d27e479e9bcea5f6bd4aec928a6177a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simple-todo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
